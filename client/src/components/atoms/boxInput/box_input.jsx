5dcfaf233b2b58b1685e9873d2bcc63d39027600
MinKyu Tae <alsrb8442@naver.com>
2022-10-06 00:42:54 +0900

    fix : front box_input's for attribute to htmlFor

--- client/src/components/atoms/boxInput/box_input.jsx
+++ client/src/components/atoms/boxInput/box_input.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Boxinput = ({ desc }) => {
   return (
     <div className="box-input">
-      <label for={desc}>{desc}</label>
+      <label htmlFor={desc}>{desc}</label>
       <input type="text" name={desc} placeholder={`${desc}을 입력하세요.`} />
     </div>
   );
