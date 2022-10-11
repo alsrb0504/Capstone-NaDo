ab155c0662e96282b246255c6919f1ea05070069
hanyounho <0@@6>
2022-10-12 02:49:40 +0900

    feat : front make social_register page
    react hook을 활용하지 못하였음, 추후에 추가 필요

--- client/src/pages/socialRegister/social_register.jsx
+++ client/src/pages/socialRegister/social_register.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import Boxinput from "../../components/atoms/boxInput/box_input";
+import Btn from "../../components/atoms/btn/btn";
 
 const SocialRegister = (props) => {
   return (
     <div className="col-sm-4 register">
-      <div>(소셜)회원가입 페이지</div>
+      <div className="social-register-nickname">
+        <Boxinput
+          desc="닉네임"
+          text="Nickname"
+        />
+      </div>
+
+      <div className="social-register-email">
+        <Boxinput
+          desc="이메일"
+          text="email"
+        />
+      </div>
+
+      <Btn
+        text="PASS로 인증"
+      />
+  
+      <div className="social-register-btn-complete">
+        <Btn
+          text="회원가입 완료"
+        />
+      </div>
     </div>
   );
 };
