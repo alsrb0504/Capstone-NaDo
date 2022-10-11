e2f4f2adeb121fc709363fe2764fbf90506a20c5
MinKyu Tae <alsrb8442@naver.com>
2022-10-12 00:47:07 +0900

    feat : front make move func to test login

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Btn from "../../components/atoms/btn/btn";
 
 const Home = (props) => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+
+  const MoveTest = () => {
+    navigate("/test/login");
+  };
+
+  return (
+    <div>
+      home
+      <Btn color="red" text="테스트 로그인 페이지" handleClick={MoveTest}></Btn>
+    </div>
+  );
 };
 
 export default Home;
