3e3f6177698800b761014191fa4dd8ee2e5c8910
violet-mj <violet_prog@naver.com>
2022-10-12 03:08:30 +0900

    feat: naver login router added

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -4,6 +4,11 @@ export default () => {
     session_secret: process.env.SESSION_SECRET,
     port: parseInt(process.env.PORT, 10) || 3000,
     database_type: process.env.NADO_DATABASE_TYPE,
+    naver_login: {
+      clientId: process.env.NAVER_LOGIN_CLIENT_ID,
+      clientSecret: process.env.NAVER_LOGIN_CLIENT_SECRET,
+      callbackUrl: process.env.NAVER_LOGIN_CALLBACK_URL
+    },
     database: {
       host: process.env.NADO_MYSQL_HOST,
       port: process.env.NADO_MYSQL_PORT || 3306,
