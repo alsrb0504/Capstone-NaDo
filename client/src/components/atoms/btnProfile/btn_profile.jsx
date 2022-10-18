c0fc39a2e0429cb18a4e0b50ce5b3ed3fbe11a16
MinKyu Tae <alsrb8442@naver.com>
2022-10-18 23:35:16 +0900

    feat : make setting page

--- client/src/components/atoms/btnProfile/btn_profile.jsx
+++ client/src/components/atoms/btnProfile/btn_profile.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const BtnProfile = ({ text, handleClick }) => {
+  const OnClick = () => {
+    handleClick();
+  };
 
-    const OnClick = () => {
-        handleClick();
-    };
-
-    return(
-        <button className="btn-profile" onClick={OnClick}>
-            <span className="name">{text}</span>
-            <i className="fa-solid fa-chevron-right"></i>
-        </button>
-    );
+  return (
+    <button type="button" className="btn-profile" onClick={OnClick}>
+      <span className="name">{text}</span>
+      <i className="fa-solid fa-chevron-right" />
+    </button>
+  );
 };
 
 export default BtnProfile;
--- client/src/pages/setting/setting.jsx
+++ client/src/pages/setting/setting.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import BtnProfile from '../../components/atoms/btnProfile/btn_profile';
 import Footer from '../../components/atoms/footer/footer';
 import Header from '../../components/atoms/header/header';
 import ProfileCard from '../../components/atoms/profileCard/profile_card';
@@ -8,20 +9,58 @@ import ProfileCard from '../../components/atoms/profileCard/profile_card';
 const Setting = () => {
   const navigate = useNavigate();
 
-  // const { userNickname } = useSelector((state) => state.user);
+  // 추후 프로필도 가져와서 업데이트
+  const { userNickname, userProvider } = useSelector((state) => state.user);
+
+  // 추후 회원탈퇴 구현
+  const UnRegister = () => {
+    alert('회원 탈퇴 미구현');
+  };
 
   const MoveHome = () => {
     navigate('/');
   };
 
+  const MoveEditProfile = () => {
+    navigate('/setting/edit');
+  };
+
+  const MoveEditPassword = () => {
+    if (userProvider === 'local') return;
+
+    navigate('/setting/password');
+  };
+
+  const MoveRecord = () => {
+    navigate('/setting/record');
+  };
+
+  const MoveProfit = () => {
+    navigate('/setting/profit');
+  };
+
   return (
     <div className="setting col-sm-4">
-      <Header handleClick={MoveHome} />
+      <Header title="설정" handleClick={MoveHome} />
 
+      {/* url 나중에 프로필 정보 받아오는 걸로 교체 */}
       <ProfileCard
         url="https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_960_720.jpg"
-        nickname="테스트 유저"
+        nickname={userNickname}
       />
+
+      <section className="setting-menu">
+        <div className="setting-menu-title">개인정보</div>
+        <BtnProfile text="프로필 수정" handleClick={MoveEditProfile} />
+        <BtnProfile text="비밀번호 변경" handleClick={MoveEditPassword} />
+        <BtnProfile text="회원 탈퇴" handleClick={UnRegister} />
+      </section>
+
+      <section className="setting-menu setting-menu-last">
+        <div className="setting-menu-title">사용 내역</div>
+        <BtnProfile text="주문 내역" handleClick={MoveRecord} />
+        <BtnProfile text="픽업 내역" handleClick={MoveProfit} />
+      </section>
 
       <Footer />
     </div>
