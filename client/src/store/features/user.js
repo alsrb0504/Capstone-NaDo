3d29f559ca49e521523d6612b95ae6c622915263
MinKyu Tae <alsrb8442@naver.com>
2022-10-22 23:54:55 +0900

    feat : image upload test code

--- client/src/store/features/user.js
+++ client/src/store/features/user.js
@@ -2,8 +2,8 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
-  // userNickname: '',    // 테스트용
-  userNickname: 'testUser',
+  userId: '',
+  userNickname: '',
   userEmail: '',
   userProvider: '',
   // userProfile: '',   // 추후 상의 후 추가해야 할 듯
@@ -16,18 +16,44 @@ const initialState = {
 // *
 // 프로필 업데이트 함수
 // 아직 reducer에 연결 X.
+// nickname
+// identifier
 // *
 export const UpdateProfile = createAsyncThunk(
   'user/UpdateProfile',
-  async ({ nickname, image }, thunkAPI) => {
+  async ({ nickname, image, identifier }, thunkAPI) => {
     console.log(nickname, image);
+
+    try {
+      const formData = new FormData();
+      formData.append('image', image[0]);
+
+      console.log(`image = ${image}`);
+      console.log(formData);
+
+      const img_res = await axios({
+        method: 'post',
+        url: 'http://localhost:3001/user/profile_image',
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(img_res);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
 
     // 추후 api 주소 정하면 교체
     try {
-      const response = await axios.post('/test', {
-        nickname,
-        image, // 아마 추가적인 조치 필요할 거 같은데 추후 구현
-      });
+      const response = await axios.post(
+        'http://localhost:3001/user/change_nickname',
+        {
+          identifier,
+          nickname,
+        },
+      );
 
       // 업데이트 된, 닉네임, 프로필 이미지 정보 받아옴.
       if (response.status === 200) {
@@ -44,6 +70,9 @@ export const UpdateProfile = createAsyncThunk(
 // *
 // 비밀번호 변경 함수
 // 아직 reducer에 연결 X.
+// 기존 비밀번호: prevPasswd
+// 새 비밀번호: newPasswd
+// 아이디: identifier
 // *
 export const ChangePasswd = createAsyncThunk(
   'user/ChangePasswd',
@@ -52,16 +81,20 @@ export const ChangePasswd = createAsyncThunk(
 
     // 추후 api 주소 정하면 교체
     try {
-      const response = await axios.post('/test', {
-        prevPasswd,
-        newPasswd,
-      });
+      const response = await axios.post(
+        'http://localhost:3001/user/change_password',
+        {
+          prevPasswd,
+          newPasswd,
+        },
+      );
 
       // 비밀번호 변경 성공
       if (response.status === 200) {
         return response.data;
       }
-      // 비밀번호 변경 실패 (원인 출력 X)
+      // 비밀번호 변경 실패 (원인 출력 X) 403?
+      alert(response.data.message);
       return thunkAPI.rejectWithValue(response.data);
     } catch (e) {
       PrintError(e, '비밀번호 변경');
