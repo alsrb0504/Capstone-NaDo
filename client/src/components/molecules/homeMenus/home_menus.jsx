7f49a5fa896c1d9f99c781abb517d79b05280ff5
hanyounho <0@@6>
2022-11-17 00:29:55 +0900

    feat : front add icon, fix text in home_menus

--- client/src/components/molecules/homeMenus/home_menus.jsx
+++ client/src/components/molecules/homeMenus/home_menus.jsx
@@ -5,9 +5,11 @@ import BtnBox from '../../atoms/buttons/btnBox/btn_box';
 const HomeMenus = () => {
   const navigate = useNavigate();
 
-  const MoveChat = () => {
-    navigate('/');
-  };
+  const MoveOrderHistory = () => navigate('/setting/order_history');
+  const MovePickupHistory = () => navigate('/setting/income_calculate');
+  const MoveSetting = () => navigate('/setting');
+  const MoveCart = () => navigate('/order/cart');
+
 
   return (
     <div className="home-menus col-sm-4">
@@ -15,23 +17,23 @@ const HomeMenus = () => {
       <div className="home-menus-btns">
         <BtnBox
           text="주문 내역"
-          url="icon/signup_icon.svg"
-          handleClick={MoveChat}
+          url="icon/order_list_icon.svg"
+          handleClick={MoveOrderHistory}
         />
         <BtnBox
-          text="주문 내역"
-          url="icon/signup_icon.svg"
-          handleClick={MoveChat}
+          text="픽업 내역"
+          url="icon/pickup_list_icon.svg"
+          handleClick={MovePickupHistory}
         />
         <BtnBox
-          text="주문 내역"
-          url="icon/signup_icon.svg"
-          handleClick={MoveChat}
+          text="설정"
+          url="icon/setting_icon.svg"
+          handleClick={MoveSetting}
         />
         <BtnBox
-          text="주문 내역"
-          url="icon/signup_icon.svg"
-          handleClick={MoveChat}
+          text="장바구니"
+          url="icon/cart_icon.svg"
+          handleClick={MoveCart}
         />
       </div>
     </div>
