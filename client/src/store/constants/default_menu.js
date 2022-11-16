0ee958ca2993428be94d848b5002c0c876575fb0
MinKyu Tae <alsrb8442@naver.com>
2022-11-17 00:12:04 +0900

    refactor : split default menu file

---
+++ client/src/store/constants/default_menu.js
@@ -0,0 +1,52 @@
+const defaultMenus = [
+  {
+    menuName: '아메리카노',
+    menuPrice: 3800,
+    menuImg: '/images/coffeeImg/americano.png',
+    sequence: 101,
+  },
+  {
+    menuName: '카페라떼',
+    menuPrice: 4300,
+    menuImg: '/images/coffeeImg/caffelatte.png',
+    sequence: 102,
+  },
+  {
+    menuName: '카페모카',
+    menuPrice: 4300,
+    menuImg: '/images/coffeeImg/caffemocha.png',
+    sequence: 103,
+  },
+  {
+    menuName: '카푸치노',
+    menuPrice: 4500,
+    menuImg: '/images/coffeeImg/cappuchino.png',
+    sequence: 104,
+  },
+  {
+    menuName: '카라멜 마끼아또',
+    menuPrice: 4500,
+    menuImg: '/images/coffeeImg/caramelmacchiato.png',
+    sequence: 105,
+  },
+  {
+    menuName: '콜드브류',
+    menuPrice: 4300,
+    menuImg: '/images/coffeeImg/coldbrew.png',
+    sequence: 106,
+  },
+  {
+    menuName: '복숭아 아이스티',
+    menuPrice: 3800,
+    menuImg: '/images/coffeeImg/peachicedtae.png',
+    sequence: 107,
+  },
+  {
+    menuName: '딸기라떼',
+    menuPrice: 4700,
+    menuImg: '/images/coffeeImg/strawberrylatte.png',
+    sequence: 108,
+  },
+];
+
+export default defaultMenus;
