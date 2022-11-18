6d977cc4bde694a4bdfc3c4541a0f2eacddbac6d
MinKyu Tae <alsrb8442@naver.com>
2022-11-18 15:31:58 +0900

    fix : order waiting value problem

--- client/src/components/atoms/cards/pickingCard/picking_card.jsx
+++ client/src/components/atoms/cards/pickingCard/picking_card.jsx
@@ -3,9 +3,9 @@ import { PrintPrice } from '../../../../utils/text';
 import { ChangeTimeInfo } from '../../../../utils/time';
 
 const PickingCard = ({ info, MoveDesc }) => {
-  const { address, pickupSequence, timeout, totalPrice } = info;
+  const { orderAddress, pickupSequence, orderTimeout, totalPrice } = info;
   // 이름 중복
-  // const { address, detail } = address;
+  const { address, detail } = orderAddress;
 
   // 주문 상세 정보로 이동
   const GoDesc = () => {
@@ -17,10 +17,10 @@ const PickingCard = ({ info, MoveDesc }) => {
       <div className="order-info" onClick={GoDesc}>
         <div className="info">
           <h3>
-            {address.address} {address.detail}
+            {address} {detail}
           </h3>
           <p>
-            마감 시간 : <span>~ {ChangeTimeInfo(timeout)}</span>
+            마감 시간 : <span>~ {ChangeTimeInfo(orderTimeout)}</span>
           </p>
           <p>
             주문 금액 : <span>{PrintPrice(totalPrice)} 원</span>
@@ -30,9 +30,7 @@ const PickingCard = ({ info, MoveDesc }) => {
         <i className="fa-solid fa-chevron-right" />
       </div>
 
-      {/* <button type="button" onClick={GoChat}> */}
       <button type="button" onClick={GoDesc}>
-        {/* 피커와 채팅하기 */}
         주문 상세 정보 보기
       </button>
     </div>
