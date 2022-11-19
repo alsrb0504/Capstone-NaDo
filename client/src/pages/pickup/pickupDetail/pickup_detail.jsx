2093c0fa4f840f8e50ab1e50853d77d6b57b93c1
MinKyu Tae <alsrb8442@naver.com>
2022-11-19 19:00:11 +0900

    fix : pickup page default value problem

--- client/src/pages/pickup/pickupDetail/pickup_detail.jsx
+++ client/src/pages/pickup/pickupDetail/pickup_detail.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import Btn from '../../../components/atoms/buttons/btn/btn';
 import Header from '../../../components/atoms/headers/header/header';
 import StateBox from '../../../components/atoms/stateBox/state_box';
@@ -12,48 +13,53 @@ const PickupDetail = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const MoveHome = () => navigate('/');
   const MoveBack = () => navigate('/pickup/store');
 
   const { isCatch, selectedOrder } = useSelector((state) => state.pickup);
   const { orderSequence, orderStatus } = selectedOrder;
 
   const AccessOrder = () => {
+    const popupTimer = 1200;
+
     // 아래 버튼에서 컷해서 아마 뜰 일이 없을 거 같긴 함.
     if (isCatch) {
-      alert('이미 진행중인 주문이 있습니다.');
+      Swal.fire({
+        title: '이미 진행중인 주문이 있습니다.',
+        text: '',
+        icon: 'error',
+        showConfirmButton: false,
+
+        timer: popupTimer,
+      });
       return;
     }
 
     dispatch(CatchPickup(orderSequence));
-    alert('픽업 성공!');
+
+    Swal.fire({
+      title: '주문 수락!',
+      text: '',
+      icon: 'success',
+      showConfirmButton: false,
+
+      timer: popupTimer,
+    });
+    // 홈으로 화면 이동
+    setTimeout(() => {
+      MoveHome();
+    }, popupTimer);
   };
-
-  // const CancelOrder = () => {
-  //   dispatch(CancelPickup(orderSequence));
-  // };
-
-  // const CompleteOrder = () => {
-  //   alert('미구현, 음.. 여기서 배달 완료가 필요한가?');
-  // };
 
   return (
     <div className="col-sm-4 pickup-detail">
       <Header title="픽업 주문 상세" handleClick={MoveBack} />
-      {/* <StateBox state={isCatch ? orderStatus : 'catched'} /> */}
+      {/* <StateBox state={isCatch ? 'ordered' : 'catched'} /> */}
 
       <PickupInfoSection selectedOrder={selectedOrder} />
 
-      {/* {isCatch && (
-        <React.Fragment>
-          <Btn text="취소하기" color="red" handleClick={CancelOrder} />
-          <Btn text="배달 완료" handleClick={CompleteOrder} />
-        </React.Fragment>
-      )} */}
-
-      {/* {!isCatch && <Btn text="픽업하기" handleClick={AccessOrder} />} */}
-      {/* {!isCatch && <Btn text="픽업하기" handleClick={AccessOrder} />} */}
-      <Btn text="픽업하기" handleClick={AccessOrder} />
-      {/* {isCatch && <Btn text="확인" handleClick={MoveBack} />} */}
+      {isCatch && <Btn text="확인" handleClick={MoveBack} />}
+      {!isCatch && <Btn text="픽업하기" handleClick={AccessOrder} />}
     </div>
   );
 };
