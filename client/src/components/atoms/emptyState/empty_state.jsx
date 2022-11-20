7446b5ca9cc738932f949924b92e3c582dd8cb3f
hanyounho <0@@6>
2022-11-20 19:10:08 +0900

    feat : add empty_state components in pickup/store

--- client/src/components/atoms/emptyState/empty_state.jsx
+++ client/src/components/atoms/emptyState/empty_state.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-const EmptyState = ({ text }) => (
-    <section className="empty-section">
-        <div className="empty-container">
-        <img className="coffee-img" src="/images/coffee.svg" alt="" />
-        </div>
-        <p className="empty-text">{text}</p>
-    </section>
-);
+const sampletext = 'store'
+
+const EmptyState = ({ text, type }) => {
+    if(type===sampletext) {
+        return (
+            <section className="empty-section-store">
+                <div>
+                    <img className="coffee-img" src="/images/coffee.svg" alt="" />  
+                </div>
+                <p>{text}</p>
+            </section>
+        );
+    } 
+    return (
+        <section className="empty-section">
+            <div className="empty-container">
+                <img className="coffee-img" src="/images/coffee.svg" alt="" />
+            </div>
+            <p className="empty-text">{text}</p>
+        </section>
+    );
+};
 export default EmptyState;
