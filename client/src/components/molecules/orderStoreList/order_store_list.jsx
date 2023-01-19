45b5afe6ab1a8095df864d3f9b6bfc0c80e699d5
MinKyu Tae <alsrb8442@naver.com>
2023-01-19 12:55:31 +0900

    fix : OrderStireList IsSame func compare problem

--- client/src/components/molecules/orderStoreList/order_store_list.jsx
+++ client/src/components/molecules/orderStoreList/order_store_list.jsx
@@ -1,11 +1,26 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable consistent-return */
 import React from 'react';
 import OrderStoreCard from '../../atoms/cards/orderStoreCard/order_store_card';
 
 function IsSame(prev, next) {
-  // eslint-disable-next-line consistent-return
   Object.keys(prev).forEach((el) => {
     if (prev[el] !== next[el]) return false;
   });
+
+  // 가게 목록 비교
+  const prevList = prev.storeList;
+  const nextList = next.storeList;
+
+  if (prevList.length !== nextList.length) return false;
+
+  for (let i = 0; i < prevList.length; i++) {
+    const { sequence, name, image } = prevList[i];
+
+    if (sequence !== nextList[i].sequence) return false;
+    if (name !== nextList[i].name) return false;
+    if (image !== nextList[i].image) return false;
+  }
 
   return true;
 }
